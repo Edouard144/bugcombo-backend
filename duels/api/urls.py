16c68ef66f220d5897f48b38997b189f7de2cf52
Edouard <edouardtuyubahe@gmail.com>
2026-08-08 01:27:07 +0200

    feat: add rematch endpoint to create new duel with same settings

--- duels/api/urls.py
+++ duels/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreateDuelView, JoinDuelView, DuelDetailView, SubmitCodeView, RoomSubmissionsView
+from .views import CreateDuelView, JoinDuelView, DuelDetailView, SubmitCodeView, RoomSubmissionsView, RematchView
 
 urlpatterns = [
     path('create/', CreateDuelView.as_view(), name='create_duel'),
     path('<str:code>/join/', JoinDuelView.as_view(), name='join_duel'),
     path('<str:code>/submit/', SubmitCodeView.as_view(), name='submit_code'),
     path('<str:code>/submissions/', RoomSubmissionsView.as_view(), name='room_submissions'),
+    path('<str:code>/rematch/', RematchView.as_view(), name='rematch'),
     path('<str:code>/', DuelDetailView.as_view(), name='duel_detail'),
 ]
