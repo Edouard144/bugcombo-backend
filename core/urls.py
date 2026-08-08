244b9192cdcf13267ce7ec910e100915147ee818
Edouard <edouardtuyubahe@gmail.com>
2026-08-08 14:10:58 +0200

    feat: add URL routes for notifications and achievements

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('api/duels/', include('duels.api.urls')),
     path('api/bugs/', include('bugs.api.urls')),
     path('api/chat/', include('chat.api.urls')),
+    path('api/notifications/', include('notifications.api.urls')),
+    path('api/achievements/', include('achievements.api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
